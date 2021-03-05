35ed5ad56253e6f50079a89285d300b77c513163
KrisTovski <kfilak@onet.eu>
2021-03-05 11:15:33 +0100

    add sha256 Password encryption to UserService

--- src/main/java/com/kristovski/sales/domain/api/UserService.java
+++ src/main/java/com/kristovski/sales/domain/api/UserService.java
@@ -2,7 +2,9 @@ package com.kristovski.sales.domain.api;
 
 import com.kristovski.sales.domain.user.User;
 import com.kristovski.sales.domain.user.UserDao;
+import org.apache.commons.codec.digest.DigestUtils;
 
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 
 public class UserService {
@@ -10,7 +12,17 @@ public class UserService {
 
     public void register(UserRegistrationDto userRegistration) {
         User userToSave = UserMapper.map(userRegistration);
-        userDao.save(userToSave);
+        try {
+            hashPasswordWithSha256(userToSave);
+            userDao.save(userToSave);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void hashPasswordWithSha256(User user) throws NoSuchAlgorithmException {
+        String sha256Password = DigestUtils.sha256Hex(user.getPassword());
+        user.setPassword(sha256Password);
     }
 
     private static class UserMapper {
